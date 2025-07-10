dbcf9bc3d5368cffee577c31aecba6567a283e3b
sukarnozhang <sukarno.zhang@gmail.com>
2025-07-10 14:30:05 +1000

    update

--- src/main/java/com/cinema_package/cinema_project/ErrorResponse.java
+++ src/main/java/com/cinema_package/cinema_project/ErrorResponse.java
@@ -6,10 +6,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Sending error details
+ * in HTTP responses when an exception occurs.
+ * Used in GlobalExceptionHAndlers
+ * Apply get() and set() method for all fields
+ * Includes:
+ * - message: A human-readable error message
+ * - timestamp: The time the error occurred
+ */
 @Getter
 @Setter
 @AllArgsConstructor
 public class ErrorResponse {
+    
+    // A brief description of the error
     private String message;
+
+    // The timestamp when the error occurred
     private LocalDateTime timestamp;
 }
